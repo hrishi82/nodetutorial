57c3623fe0945f8a25dd831e2ba93428de467f0a
hrishi82 <hrishib4882@gmail.com>
2023-10-05 09:53:49 +0530

    chap 6

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
-const http = require('http')
+const express = require('express')
+const app  = express()
 const path = require('path')
-const fs = require('fs')
-const fsPromises = require('fs').promises
 
-const {logEvents} = require('./logEvents')
-const EventEmitter = require('events')
-
-// From the docs: https://nodejs.dev/en/api/v20/events/
-class Emitter extends EventEmitter {}
-const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method)
+app.get('^/$|index(.html)?', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views", 'index.html'))
+})
+app.get('/new-page(.html)?', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views", 'new-page.html'))
 })
 
-server.listen(PORT, ()=> console.log('server running on port: ', PORT))
+app.get('/hello-world', (req, res, next)=>{
+    console.log('hello world console')
+    next()
+}, (req,res)=>{
+    res.send('Hello world!')
+})
+
+const one = (req, res, next) =>{
+    console.log('one')
+    next()
+}
+const two = (req, res, next) =>{
+    console.log('two')
+    next()
+}
+const three = (req, res, next) =>{
+    console.log('three - finished')
+}
+
+app.get('/chain', [one, two, three])
+
+app.get('/old-page(.html)?', (req,res)=>{
+    res.redirect(301, './new-page')
+})
+app.get('/*', (req,res)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(PORT, ()=> console.log('server running on port: ', PORT))
